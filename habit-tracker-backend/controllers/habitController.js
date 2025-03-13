3ce56df58a866767755a10f7462753372c218b42
Waseef Mohammad Khan <waseef2010@gmail.com>
2025-03-13 06:35:54 +0000

    update createdAt to be a locale string, i.e. date and time

--- habit-tracker-backend/controllers/habitController.js
+++ habit-tracker-backend/controllers/habitController.js
@@ -11,7 +11,7 @@ const createHabit = (req, res) => {
     name: req.body.name,
     habit: req.body.habit,
     completed: false,
-    createdAt: new Date().toISOString(),
+    createdAt: new Date().toLocaleString(),
   };
 
   habits.push(newHabit);
